44638d40e64f84c18f66c520da164ff4872bbd0b
omarstate <omarstate@ymail.com>
2025-04-25 09:14:59 +0300

    Added the intended implementation.

---
+++ app.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+function askForNames() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const names = [];
+
+    function ask() {
+        rl.question('Enter a name (or type "done" to finish): ', (answer) => {
+            if (answer.toLowerCase() === 'done') {
+                console.log('Names:', names);
+                rl.close();
+            } else {
+                names.push(answer);
+                ask();
+            }
+        });
+    }
+
+    ask();
+}
+
+askForNames();
